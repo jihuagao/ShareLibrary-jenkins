37729aa814709bf082951b1dcaffd77e5b0153f4
jihuagao <40812101+jihuagao@users.noreply.github.com>
2020-08-10 17:46:36 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -5,12 +5,10 @@ package org.devops
 def Build(buildType,buildShell){
     def buildTools = ["mvn":"M3","npm":"NPM"]
     
-    
     println("当前选择的构建类型为 ${buildType}")
     buildHome= tool buildTools[buildType]
     
     if ("${buildType}" == "npm"){
-        
         sh  "${buildHome}/bin/${buildType} ${buildShell}"
     } else {
         sh "${buildHome}/bin/${buildType} ${buildShell}"
